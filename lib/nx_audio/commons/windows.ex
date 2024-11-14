6802202800c5f1771ac3af134dcb6e9eed4850f6
Ygor Castor <ygorcastor@gmail.com>
2024-11-14 22:09:53 +0100

    Implemented spectrogram transformation
    Also simplified the window module

--- lib/nx_audio/commons/windows.ex
+++ lib/nx_audio/commons/windows.ex
@@ -1,17 +1,20 @@
-defmodule NxAudio.Commons.Window.Hann do
+defmodule NxAudio.Commons.Windows do
+  @moduledoc """
+  NX Implementation of common window functions.
+  """
   import Nx.Defn
 
   @pi 3.141592653589793
 
   @doc """
-  Creates a Hann window of size `window_length`.
+  Creates a Hann window of size `window_length`.  
 
   Args:
-    window_length: Length of the window. Must be positive integer.
-    periodic: If true, returns a periodic window for use with FFT. Defaults to true.
+    window_length: Length of the window. Must be positive integer.  
+    periodic: If true, returns a periodic window for use with FFT. Defaults to true.  
 
   Returns:
-    A 1-D tensor of size (window_length,) containing the window
+    A 1-D tensor of size (window_length,) containing the window  
 
   ## Examples
       iex> Hann.create(window_length: 4)
@@ -20,13 +23,13 @@ defmodule NxAudio.Commons.Window.Hann do
         [0.0, 0.5, 0.5, 0.0]
       >
   """
-  defn create(opts \\ []) do
-    opts = keyword!(opts, window_length: 1, periodic: 1)
+  defn haan(opts) do
+    opts = keyword!(opts, window_length: 1, periodic: true)
     window_length = opts[:window_length]
     periodic = opts[:periodic]
 
     # For periodic windows, we compute N+1 points and discard the last one
-    computation_length = if periodic == 1, do: window_length + 1, else: window_length
+    computation_length = if periodic, do: window_length + 1, else: window_length
 
     n = Nx.iota({computation_length})
     window = 0.5 - 0.5 * Nx.cos(2 * @pi * n / (computation_length - 1))
---
+++ lib/nx_audio/transforms/spectrogram_config.ex
@@ -0,0 +1,105 @@
+defmodule NxAudio.Transforms.SpectrogramConfig do
+  @moduledoc """
+  Configuration options for spectrogram transformation.
+  """
+  @schema NimbleOptions.new!(
+            n_fft: [
+              type: :non_neg_integer,
+              default: 400,
+              doc: "Size of FFT, creates n_fft // 2 + 1 bins."
+            ],
+            win_length: [
+              type: :non_neg_integer,
+              doc: "Number of samples in each frame. By default its half of the n_fft"
+            ],
+            hop_length: [
+              type: :non_neg_integer,
+              doc: """
+              Number of samples between successive frames., By default its half of the n_fft
+              """
+            ],
+            pad: [
+              type: :non_neg_integer,
+              default: 0,
+              doc: "Two sided padding of signal."
+            ],
+            window_fn: [
+              default: &NxAudio.Commons.Windows.haan/1,
+              doc: "Window function to apply to each frame."
+            ],
+            power: [
+              type: :float,
+              default: 2,
+              doc: """
+              Exponent for the magnitude spectrogram, (must be > 0) e.g., 1 for magnitude, 2 for power, etc. If None, then the complex spectrum is returned instead. 
+              """
+            ],
+            normalized: [
+              type: {:in, [:window, :frame_length]},
+              doc: """
+              Whether to normalize by magnitude after stft. choices are "window" and "frame_length", if specific normalization type is desirable.
+              """
+            ],
+            wkwargs: [
+              type: :keyword_list,
+              default: [],
+              doc: "Arguments for window function."
+            ],
+            center: [
+              type: :boolean,
+              default: true,
+              doc: """
+              Whether to pad waveform on both sides so that the t-th frame is centered at time t * hop_length.
+              """
+            ],
+            pad_mode: [
+              type: :atom,
+              default: :reflect,
+              doc: """
+              Controls the padding method used when center is true.
+              """
+            ],
+            onesided: [
+              type: :boolean,
+              default: true,
+              doc: """
+              Controls whether to return half of results to avoid redundancy.
+              """
+            ]
+          )
+
+  import Nx.Defn
+
+  @typedoc """
+  #{NimbleOptions.docs(@schema)}
+  """
+  @type t() :: [unquote(NimbleOptions.option_typespec(@schema))]
+
+  @doc """
+  Parses and validate a keyword list into a valid spectrogram config
+  """
+  defn validate(config) do
+    config
+    |> keyword!(
+      n_fft: 400,
+      win_length: -1,
+      hop_length: -1,
+      pad: 0,
+      window_fn: &NxAudio.Commons.Windows.haan/1,
+      power: 2.0,
+      normalized: nil,
+      wkwargs: [],
+      center: true,
+      pad_mode: :reflect,
+      onesided: true
+    )
+  end
+
+  defn maybe_calculate_length(config, which_length) do
+    if config[which_length] != -1 do
+      config[which_length]
+    else
+      div(config[:n_fft], 2)
+    end
+  end
+end
